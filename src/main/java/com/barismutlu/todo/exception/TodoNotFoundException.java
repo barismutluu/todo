020473483cb735eae67b2d0b764c3779d0275cbe
Barış Mutlu <105866654+barismutluu@users.noreply.github.com>
2025-01-11 22:49:36 +0300

    TodoNotFoundException

--- src/main/java/com/barismutlu/todo/exception/TodoNotFoundException.java
+++ src/main/java/com/barismutlu/todo/exception/TodoNotFoundException.java
@@ -1,6 +1,8 @@
 package com.barismutlu.todo.exception;
 
 public class TodoNotFoundException extends RuntimeException {
+
+    //kurucu metot, özel hata mesajların tanımlanması.
     public TodoNotFoundException(String message) {
         super(message);
     }
