1c51f55b79a71104e0feaf010ab2771c48eb3e42
Barış Mutlu <105866654+barismutluu@users.noreply.github.com>
2025-01-11 19:46:39 +0300

    GlobalExceptionHandler

---
+++ src/main/java/com/barismutlu/todo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.barismutlu.todo.exception;
+
+import com.barismutlu.todo.exception.TodoNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TodoNotFoundException.class)
+    public ResponseEntity<String> handleTodoNotFoundException(TodoNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Hata: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Bir hata oluştu. Lütfen daha sonra tekrar deneyin.");
+    }
+}
