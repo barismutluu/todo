f05f01387e182b898569bf3e59c501a3753d50d3
Barış Mutlu <105866654+barismutluu@users.noreply.github.com>
2025-01-11 18:59:12 +0300

    TodoRepository

---
+++ src/main/java/com/barismutlu/todo/data/repository/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.barismutlu.todo.data.repository;
+
+import com.barismutlu.todo.data.entity.TodoItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoItem,Long> {
+
+}
