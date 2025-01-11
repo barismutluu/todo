f5d77fd4e16d61aedcf41655773347ff47c869e2
Barış Mutlu <105866654+barismutluu@users.noreply.github.com>
2025-01-11 22:49:14 +0300

    TodoDTO

--- src/main/java/com/barismutlu/todo/business/dto/TodoDTO.java
+++ src/main/java/com/barismutlu/todo/business/dto/TodoDTO.java
@@ -3,10 +3,10 @@ package com.barismutlu.todo.business.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
-@Getter
+@Setter//metotlar için tanımlanmış setter
+@Getter//metotlar için tanımlanmış getter
 public class TodoDTO {
-
+// proje için gerekli kurucu metotları
     private Long id;
     private String description;
     private boolean completed;
